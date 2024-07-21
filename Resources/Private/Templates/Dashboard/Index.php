24eb9955a9478d0d841ea6733aad0c19c3c29928
floriankuehl <florian@fksweb.de>
2024-07-21 12:09:35 +0200

    Create Index.php
    Added a simple user greeting for templating demonstration

---
+++ Resources/Private/Templates/Dashboard/Index.php
@@ -0,0 +1,8 @@
+<p class="demo">
+  Hello <b>{user.salutation}</b> <b>{user.lastName}</b>, or may I call you <b>{user.firstName}</b>?<br>
+  <br>
+  Anyway, you are living in the <b>{user.address}</b> No. <b>{user.address2}</b>, right?<br> 
+  I found this street in <b>{user.zip}</b> <b>{user.city}</b> located in <b>{user.country}</b>.<br>
+  <br>
+  I will send a mail to <b>{user.email}</b> or call <b>{user.tel}</b> if something may occur.
+</p>
