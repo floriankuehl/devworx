ca587937040be4e0d304a0c989f71be2b450f92e
Florian Kühl <florian@fksweb.de>
2025-04-17 16:51:49 +0200

    Update StringUtility.php
    added comments

--- Classes/Devworx/Utility/StringUtility.php
+++ Classes/Devworx/Utility/StringUtility.php
@@ -4,22 +4,46 @@ namespace Devworx\Utility;
 
 class StringUtility {
   
-  public static function cleanup(string $value){
+  /**
+   * Cleans a string from non-alphanumeric characters
+   *
+   * @param string $value The given value
+   * @return string
+   */
+  public static function cleanup(string $value): string {
     return preg_replace('/[^\da-z]/i', '', $value);
   }
   
+  /**
+   * Checks if the given string is a MD5 hash of 32B
+   *
+   * @param string $md5 The given value
+   * @return bool
+   */
   public static function isMd5(string $md5=''): bool {
     return (bool) preg_match('/^[a-f0-9]{32}$/', $md5);
   }
   
+  /**
+   * Checks if the given string is a Base64 string
+   *
+   * @param string $value The given value
+   * @return bool
+   */
   public static function isBase64(string $base64=''): bool {
     return (bool) preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $base64);
   }
   
-  public static function phone(string $value){
+  /**
+   * Strips a phone number of formatting characters (german)
+   *
+   * @param string $value The given value
+   * @return string
+   */
+  public static function phone(string $value): string {
     $value = str_replace( 
       [' ','-','/','(0)'], 
-      ['','','',''],
+      ['','','','0'],
       $value 
     );
     if( substr($value,0,1) == '0' )
@@ -27,7 +51,13 @@ class StringUtility {
     return $value;
   }
   
-  public static function url(string $value){
+  /**
+   * Builds a valid url from a given string
+   *
+   * @param string $value The given value
+   * @return string
+   */
+  public static function url(string $value): string {
     $matches = [];
     if( preg_match("/http([s]?):\/\/([a-zA-Z\-\_\/\.]{1,})([\?]?[a-zA-Z\=\&\;]{1,})/m",$value,$matches) !== FALSE ){
       $value = $matches[2];
