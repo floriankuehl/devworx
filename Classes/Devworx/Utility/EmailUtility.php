1310c98c8662c90bd665b6972a72e0441a503913
Florian Kühl <florian@fksweb.de>
2025-04-17 16:22:37 +0200

    Update EmailUtility.php

--- Classes/Devworx/Utility/EmailUtility.php
+++ Classes/Devworx/Utility/EmailUtility.php
@@ -4,8 +4,15 @@ namespace Devworx\Utility;
 
 class EmailUtility {
   
-  
-  public static function mailtoUrl(string $receiver,string $subject='',string $body=''){
+  /**
+   * Creates a mailto url for frontend use
+   * 
+   * @param string $receiver The receiver of the mail
+   * @param string $subject The subject of the mail
+   * @param string $body The body of the mail
+   * @return string
+   */
+  public static function mailtoUrl(string $receiver,string $subject='',string $body=''): string {
     $subject = rawurlencode($subject);
     $body = rawurlencode($body);
     return "mailto:{$receiver}?subject={$subject}&body={$body}";
