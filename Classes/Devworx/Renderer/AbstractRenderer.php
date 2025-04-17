1723895bf8c22eb4f3fb6ba71dafad06ee895409
Florian Kühl <florian@fksweb.de>
2025-04-17 15:00:15 +0200

    Update AbstractRenderer.php
    added comments

--- Classes/Devworx/Renderer/AbstractRenderer.php
+++ Classes/Devworx/Renderer/AbstractRenderer.php
@@ -5,13 +5,24 @@ namespace Devworx\Renderer;
 use Devworx\Frontend;
 use Devworx\Utility\ArrayUtility;
 
+
+/**
+ * Interface IRenderer
+ * Basic interface for every renderer
+ */
 interface IRenderer {
-  static function render($source,array $variables,string $encoding);
+  static function render($source,array $variables,string $encoding): void;
 }
 
 abstract class AbstractRenderer implements IRenderer {
-  
-  abstract static function render($source,array $variables,string $encoding);
+ 
+  /**
+   * Function render
+   * Abstract function for rendering anything
+   * 
+   * @return void
+   */
+  abstract static function render($source,array $variables,string $encoding): void;
   
 }
 
