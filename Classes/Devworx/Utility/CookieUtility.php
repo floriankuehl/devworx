7a9961845d62795551e6ab65067107cf082902f9
Florian Kühl <florian@fksweb.de>
2025-04-17 16:17:23 +0200

    Update CookieUtility.php
    added comments

--- Classes/Devworx/Utility/CookieUtility.php
+++ Classes/Devworx/Utility/CookieUtility.php
@@ -8,16 +8,32 @@ class CookieUtility {
     COOKIE = 'devworx',
     EXPIRES = 3600 * 24;
   
-  static function getProgramFolder(){
+  /**
+   * Gets the folder of the current script
+   *
+   * @return string
+   */
+  static function getProgramFolder(): string {
     $tokens = explode('/',$_SERVER['SCRIPT_NAME']);
     array_pop($tokens);
     return implode('/',$tokens);
   }
   
+  /**
+   * Checks if the devworx cookie is set
+   *
+   * @return bool
+   */
   static function has(): bool {
     return array_key_exists(self::COOKIE,$_COOKIE);
   }
   
+  /**
+   * Sets the devworx cookie value
+   *
+   * @param string $value The cookie value to set (user hash)
+   * @return bool
+   */
   static function set(string $value): bool {
     return empty($value) ? false : setcookie(
       self::COOKIE,
@@ -32,14 +48,29 @@ class CookieUtility {
     );
   }
   
+  /**
+   * Refreshs the devworx cookie
+   *
+   * @return bool
+   */
   static function refresh(): bool {
     return self::set( self::get() );
   }
   
+  /**
+   * Gets the devworx cookie value
+   *
+   * @return string|null
+   */
   static function get(): ?string {
     return self::has() ? $_COOKIE[self::COOKIE] : null;
   }
   
+  /**
+   * Unsets the devworx cookie by altering the expire date
+   *
+   * @return bool
+   */
   static function unset(): bool {
     return setcookie(
       self::COOKIE,
