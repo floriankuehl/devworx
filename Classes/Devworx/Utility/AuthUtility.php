c4b61cbf69558b81026168513bd6ee45316e532c
Florian Kühl <florian@fksweb.de>
2025-04-17 16:02:14 +0200

    Update AuthUtility.php

--- Classes/Devworx/Utility/AuthUtility.php
+++ Classes/Devworx/Utility/AuthUtility.php
@@ -6,6 +6,12 @@ use \Api\Utility\ApiUtility;
 
 class AuthUtility {
   
+  /**
+   * Checks if a given string is MD5 and checks if a user with this login exists
+   *
+   * @param string $hash The login hash
+   * @return bool
+   */
   public static function isUserHash(string $hash): bool {
     global $DB;
     if( StringUtility::isMd5($hash) ){
@@ -15,6 +21,12 @@ class AuthUtility {
     return false;
   }
   
+  /**
+   * Retrieves a user by its login hash
+   *
+   * @param string $hash The user login hash
+   * @return array|null
+   */
   public static function getUserByHash(?string $hash): ?array{
     if( is_null($hash) || empty($hash) )
       return null;
@@ -23,6 +35,12 @@ class AuthUtility {
     return $DB->query("SELECT * FROM user WHERE login='{$hash}' LIMIT 1;",true,MYSQLI_ASSOC);
   }
   
+  /**
+   * Sets the last login timestamp of a user by hash
+   *
+   * @param string $hash The user login hash
+   * @return bool
+   */
   public static function setLastLogin(?string $hash): bool {
     if( is_null($hash) || empty($hash) )
       return false;
@@ -31,22 +49,42 @@ class AuthUtility {
     return (bool)$DB->query("UPDATE user SET lastLogin=CURRENT_TIMESTAMP WHERE login='{$hash}' LIMIT 1;",true,MYSQLI_NUM);
   }
   
+  /**
+   * Retrieves the current user hash by the given context
+   *
+   * @return string|null
+   */
   public static function getStoredHash(): ?string {
     return \Devworx\Frontend::isApiContext() ? 
       ApiUtility::getKey() : 
       CookieUtility::get();
   }
   
+  /**
+   * Retrieves the current user by the stored hash
+   *
+   * @return array|null
+   */
   public static function getCurrentUser(): ?array {
     $hash = self::getStoredHash();
     return is_null($hash) ? $hash : self::getUserByHash($hash);
   }
   
+  /**
+   * Checks if a devworx cookie exists
+   *
+   * @return bool
+   */
   public static function cookie(): bool {
     $hash = CookieUtility::get();
     return is_string($hash) && !empty($hash);
   }
-    
+  
+  /**
+   * Performs a login based on POST data
+   *
+   * @return bool
+   */
   public static function post():bool {
     $result = $_SERVER['REQUEST_METHOD'] === 'POST';
     if( $result ){
@@ -66,7 +104,12 @@ class AuthUtility {
     return $result;
   }
   
-  public static function lock(){
+  /**
+   * Performs a logout by destroying the session and the cookie
+   *
+   * @return void
+   */
+  public static function lock(): void {
     CookieUtility::unset();
     if( !empty(session_id()) )
       session_destroy();
