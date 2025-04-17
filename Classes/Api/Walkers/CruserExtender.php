8f1159bd796927eb47d9bb3f504ad3fd351c47f0
Florian Kühl <florian@fksweb.de>
2025-04-17 14:54:20 +0200

    Update CruserExtender.php
    Added comments

--- Classes/Api/Walkers/CruserExtender.php
+++ Classes/Api/Walkers/CruserExtender.php
@@ -10,25 +10,36 @@ class CruserExtender extends \Devworx\Walkers\AbstractSubsetWalker {
     parent::__construct(...$arguments);
   }
   
+  /**
+   * Function getSubset
+   *
+   * Retrieves a given subset
+   *
+   * @param array $list The list that will be extended
+   * @return array
+   */
   public function getSubset(array &$list): array {
     $repository = new \Devworx\Repository('user','');
     $uids = array_unique( array_column( $list, 'cruser' ) );
     $result = $repository->filter([
       'uid' => $uids,
     ],...$this->arguments);
-    /*
-    echo \Devworx\Utility\DebugUtility::var_dump([
-      'uids' => $uids,
-      'arguments' => $this->arguments,
-      'result' => $result,
-      'index' => ArrayUtility::index($result,'uid')
-    ],__CLASS__,__METHOD__,__LINE__);
-    */
+    
     $result []= \Devworx\Frontend::SYSTEM_USER;
     
     return ArrayUtility::index($result,'uid');
   }
   
+  /**
+   * Function Step
+   *
+   * Performs a step of the subset walker to extend the given row
+   *
+   * @param array $list The list that will be extended
+   * @param mixed $index The current row index
+   * @param mixed $row The current row
+   * @return void
+   */
   public function Step(array &$list,$index,&$row): void {
     $row['cruserUid'] = $row['cruser'];
     $row['cruser'] = $this->subset[ $row['cruser'] ];
