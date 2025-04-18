8448dfb18fa730f13baca9b0b1e9d04d76c56932
Florian Kühl <florian@fksweb.de>
2025-04-18 21:18:59 +0200

    Create Navigation.php

---
+++ Resources/Frontend/Partials/Navigation.php
@@ -0,0 +1,5 @@
+<nav>
+  <a href="?controller=Dashboard&action=index">Dashboard</a>
+  <a href="?controller=Dashboard&action=profile">Profile</a>
+  <a href="?controller=Login&action=logout">Logout</a>
+</nav>
