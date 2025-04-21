1a668b1ed489853b50205402640f1d459483c3e5
Florian Kühl <florian@fksweb.de>
2025-04-21 10:51:05 +0200

    Add files via upload

---
+++ Classes/Devworx/Interfaces/IModel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Devworx;
+
+/**
+ * The interface for models
+ */
+interface IModel {
+  
+  static function empty(...$args): object;
+  static function emptyArray(...$args): array;
+  static function presetArray(array $preset): array;
+  function fields(): array;
+  
+  function toArray(): array;
+  function getUid(): int;
+  function setUid(int $value): void;
+  function getCruser(): int;
+  function setCruser(int $value): void;
+  function getCreated(): ?\DateTime;
+  function setCreated(?string $value): void;
+  function getUpdated(): ?\DateTime;
+  function setUpdated(?string $value): void;
+  function getDeleted(): ?\DateTime;
+  function setDeleted(?string $value): void;
+  function getHidden(): bool;
+  function setHidden(bool $value): void;
+}
+
+?>
