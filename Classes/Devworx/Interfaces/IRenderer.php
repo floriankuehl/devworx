b3b11c236b808f06b9d2a2998eb586432a4e71e5
Florian Kühl <florian@fksweb.de>
2025-05-07 07:59:39 +0200

    Update IRenderer.php

--- Classes/Devworx/Interfaces/IRenderer.php
+++ Classes/Devworx/Interfaces/IRenderer.php
@@ -7,11 +7,11 @@ interface IRenderer
     /**
      * Returns a rendered representation of the given template
      *
-     * @param \mixed $template Template-Definition (String, Array, Objekt etc.)
+     * @param mixed $template Template-Definition (String, Array, Objekt etc.)
      * @param array $variables Context variables
-     * @return \mixed Rendered representation
+     * @return mixed Rendered representation
      */
-    public function render(\mixed $template, array $variables): \mixed;
+    public function render(mixed $template, array $variables): mixed;
 
 
 	/**
@@ -25,8 +25,8 @@ interface IRenderer
 	/**
      * Checks if the given template is supported
      *
-     * @param \mixed $template Given template (string, object, array etc)
+     * @param mixed $template Given template (string, object, array etc)
      * @return bool
      */
-	public function supports(\mixed $template): bool;
+	public function supports(mixed $template): bool;
 }
