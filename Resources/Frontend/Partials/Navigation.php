a29dd0ce68a466256039fcbde87640efd03d3eff
Florian Kühl <florian@fksweb.de>
2025-05-07 09:21:46 +0200

    Update Navigation.php

--- Resources/Frontend/Partials/Navigation.php
+++ Resources/Frontend/Partials/Navigation.php
@@ -1,5 +1,6 @@
-<nav>
-  <a href="?controller=Dashboard&action=index">Dashboard</a>
-  <a href="?controller=Dashboard&action=profile">Profile</a>
-  <a href="?controller=Login&action=logout">Logout</a>
+<nav class="d-flex flex-row flex-wrap p-2 gap-2">
+  <a class="btn btn-secondary" href="?controller=Dashboard&action=index">Dashboard</a>
+  <a class="btn btn-secondary" href="?controller=Dashboard&action=profile">Profile</a>
+  <div class="flex-grow-1">&nbsp;</div>
+  <a class="btn btn-secondary" href="?controller=Login&action=logout">Logout</a>
 </nav>
