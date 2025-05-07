b7aa04aeb9ca559b566b053084042bf5e5470cd8
Florian Kühl <florian@fksweb.de>
2025-05-07 12:05:34 +0200

    Update Module.js
    Revamped registering

--- Public/Resources/Scripts/Devworx/Module.js
+++ Public/Resources/Scripts/Devworx/Module.js
@@ -5,16 +5,36 @@ import * as Elements from './Elements.js'
 import * as ViewHelpers from './ViewHelpers.js'
 import * as ProjectElements from './ProjectElements.js'
 
-function autoRegisterClasses(module) {
-  Object.keys(module).forEach((exportedKey) => {
-    const exportedValue = module[exportedKey];
-	if (typeof exportedValue === 'function' && exportedValue.prototype instanceof HTMLElement) {
-      exportedValue.register();
-      console.log(`Registered: ${exportedValue.name}`);
-    }
+function getBaseClass(constructor) {
+  let proto = constructor.prototype;
+  let prev = null;
+
+  while (proto && proto !== HTMLElement.prototype) {
+    prev = proto;
+    proto = Object.getPrototypeOf(proto);
+  }
+
+  if (!prev) return null;
+  return prev.constructor.name;
+}
+
+function registerCustomElements(module) {
+  Object.keys(module).forEach((key) => {
+    const value = module[key];
+	
+	if (
+	  ( typeof value === 'function' ) &&
+	  ( value.prototype instanceof HTMLElement ) &&
+	  ( typeof value.register === 'function' ) &&
+	  ( typeof value.elementTag === 'string' ) &&
+	  ( !customElements.get(value.elementTag) )
+	) {
+		value.register();
+		//console.log(`Added ${getBaseClass(value)} ${value.name} as ${value.elementTag}`);	
+	}
   });
 }
 
-autoRegisterClasses(Elements)
-autoRegisterClasses(ViewHelpers)
-autoRegisterClasses(ProjectElements)
+registerCustomElements(Elements)
+registerCustomElements(ViewHelpers)
+registerCustomElements(ProjectElements)
