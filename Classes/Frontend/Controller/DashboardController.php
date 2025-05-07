7f2600f1afde39a551218f11f60f8b93fdf5011e
Florian Kühl <florian@fksweb.de>
2025-05-07 08:59:06 +0200

    Update DashboardController.php

--- Classes/Frontend/Controller/DashboardController.php
+++ Classes/Frontend/Controller/DashboardController.php
@@ -3,22 +3,25 @@
 namespace Frontend\Controller;
 
 use \Devworx\Frontend;
-//use \Devworx\Utility\FlashMessageUtility;
+use \Devworx\Utility\FlashMessageUtility;
+use \Devworx\Utility\ModelUtility;
+use \Frontend\Models\User;
 
 class DashboardController extends \Devworx\AbstractController {
-
-    public function initialize(){
+    
+	
+	public function initialize(){
 		
 	}
-    
-    public function indexAction(){
-        $user = Frontend::getCurrentUser();
-        $this->view->assign('user',$user);
-    }
-    
-    public function profileAction(){
-        $user = Frontend::getCurrentUser();
-        $this->view->assign('user',$user);
-    }
+	
+	public function indexAction(){
+		$user = Frontend::getCurrentUser();
+		$this->view->assign('user',$user);
+	}
+
+	public function profileAction(){
+		$user = ModelUtility::toModel( Frontend::getCurrentUser(), User::class );
+		$this->view->assign('user',$user);
+	}
   
 }
