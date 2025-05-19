d9968a4b847396dc23f8cc75e6f393ed74710334
Florian Kühl <florian@fksweb.de>
2025-05-19 23:18:16 +0200

    Add files via upload

--- Classes/Devworx/Interfaces/IRequest.php
+++ Classes/Devworx/Interfaces/IRequest.php
@@ -6,20 +6,27 @@ namespace Devworx\Interfaces;
  * The interface for requests
  */
 interface IRequest {
+  function getMethod(): string;
+  function getBody(): string;
+  
+  function isGet(): bool;
+  function isPost(): bool;
+  function isPut(): bool;
+  
+  function Get(string $key=null): ?mixed;
+  function Post(string $key=null): ?mixed;
+  function Put(string $key=null): ?mixed;
+  
   function getArguments(): array;
   function hasArgument(string $key): bool;
   function getArgument(string $key);
-  function getMethod(): string;
-  function getBody(): string;
   
   function getFiles(string $key): array;
   function hasFiles(string $key): bool;
   function hasFile(string $key,int $index): bool;
   function getFile(string $key,string $subkey,int $index);
   
-  function isGet(): bool;
-  function isPost(): bool;
-  function isPut(): bool;
+  
 }
 
 ?>
