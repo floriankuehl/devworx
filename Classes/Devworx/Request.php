a45f93ffffb3c6812aeb842d183428e6397da0d7
Florian Kühl <florian@fksweb.de>
2025-05-19 22:56:50 +0200

    Added interface methods

--- Classes/Devworx/Request.php
+++ Classes/Devworx/Request.php
@@ -9,10 +9,12 @@ class Request implements IRequest {
   
   protected $arguments = null;
   protected $method = null;
+  protected $files = null;
   
   function __construct(){
     $this->arguments = $_REQUEST;
     $this->method = $_SERVER['REQUEST_METHOD'];
+	$this->files = $_FILES;
   }
   
   /**
@@ -81,6 +83,68 @@ class Request implements IRequest {
     if( $this->hasArgument($key) )
       return $this->arguments[$key];
     return $fallback;
+  }
+  
+  /**
+   * Gets the current upload files
+   *
+   * @return array
+   */
+  function getFiles(): array {
+    return $this->files;
+  }
+  
+  /**
+   * Checks if a specific upload file list exists
+   *
+   * @param string $key The key of the $_FILES array
+   * @return bool
+   */
+  function hasFiles(string $key): bool {
+	if( empty($this->files) )
+		return false;
+	return ArrayUtility::key($this->files,$key);
+  }
+  
+  /**
+   * Checks if a specific upload file exists
+   *
+   * @param string $key The name of the file list
+   * @param int $index The index of the file row
+   * @return bool
+   */
+  function hasFile(string $key,int $index): bool {
+	  if( $this->hasFiles( $key ) ){
+		return ArrayUtility::isIndex($this->files['name'],$index);
+	  }
+	  return false;
+  }
+  
+  /**
+   * Retrieves a file if it exists
+   * if $subkey is empty, full data is returned as an associative array
+   *
+   * @param string $key The name of the argument
+   * @param string $subkey The file optional info key (e.g. 'name').
+   * @param int $index The index of the file row
+   * @return mixed
+   */
+  function getFile(string $key,string $subkey,int $index): mixed {
+    if( $this->hasFile($key,$index) ){
+		if( empty($subkey) ){
+			$file = [];
+			foreach( $this->files[$key] as $k => $v )
+				$file[$k] = $v[$index];
+			return $file;
+		}
+		
+		if( ArrayUtility::has($this->files,$subkey) )
+			return $this->files[$key][$subkey][$index];
+		
+		if( ArrayUtility::key($this->files,$key,$subkey)
+      return $this->files[$key][$subkey];
+	}
+	return null;
   }
   
   /**
