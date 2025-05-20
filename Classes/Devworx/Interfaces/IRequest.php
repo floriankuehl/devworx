25c0da380ef8df272a8cafd7955148c076dac136
Florian Kühl <florian@fksweb.de>
2025-05-20 10:42:51 +0200

    Add files via upload
    added signatures for setters

--- Classes/Devworx/Interfaces/IRequest.php
+++ Classes/Devworx/Interfaces/IRequest.php
@@ -13,15 +13,21 @@ interface IRequest {
   function isPost(): bool;
   function isPut(): bool;
   
-  function Get(string $key=null): ?mixed;
-  function Post(string $key=null): ?mixed;
-  function Put(string $key=null): ?mixed;
+  function Get(string $key=null): mixed;
+  function Post(string $key=null): mixed;
+  function Put(string $key=null): mixed;
+  
+  function setGet(string $key, mixed $value): void;
+  function setPost(string $key, mixed $value): void;
+  function setPut(string $key, mixed $value): void;
+  function setRequest(string $key, mixed $value): void;
   
   function getArguments(): array;
   function hasArgument(string $key): bool;
-  function getArgument(string $key);
+  function getArgument(string $key): mixed;
+  function setArgument(string $key,mixed $value): void;
   
-  function getFiles(string $key): array;
+  function getFiles(string $key): ?array;
   function hasFiles(string $key): bool;
   function hasFile(string $key,int $index): bool;
   function getFile(string $key,string $subkey,int $index);
