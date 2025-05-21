13a423f1b344d506097b3b5da6ed8b851416a945
Florian Kühl <florian@fksweb.de>
2025-05-21 23:36:17 +0200

    Add files via upload

--- Classes/Documentation/Controller/DocumentationController.php
+++ Classes/Documentation/Controller/DocumentationController.php
@@ -4,8 +4,11 @@ namespace Documentation\Controller;
 
 use \Devworx\Frontend;
 use \Devworx\Utility\FileUtility;
+use \Devworx\Utility\ArrayUtility;
 
 class DocumentationController extends \Devworx\AbstractController {
+	
+	const SOURCE_PATH = 'Configuration';
 	
 	const MIME = [
 		'js' => 'text/javascript',
@@ -23,26 +26,33 @@ class DocumentationController extends \Devworx\AbstractController {
 		$this->view->setRenderer('');
 	}
 	
+	private function path(string $fileName){
+		return realpath( Frontend::path($fileName) );
+	}
+	
+	protected function getDoxygenConfig(){
+		return Frontend::getConfig('doxygen');
+	}
+	
 	protected function prepareDoxyfile(): string {
 		
-		$doxygen = Frontend::getConfig('doxygen');
-		$constants = realpath( Frontend::path($doxygen['constants']) );
-		$output = dirname( $constants ) . DIRECTORY_SEPARATOR . $doxygen['doxyfile'];
+		$doxygen = $this->getDoxygenConfig();
+		
+		$constants = Frontend::realPath( $doxygen['workdir'], $doxygen['constants'] );
+		$warnings = Frontend::realPath( $doxygen['workdir'], $doxygen['warnings'] );
+		$doxyfile = Frontend::realPath( $doxygen['workdir'], $doxygen['doxyfile'] );
 		
 		$project = $doxygen['project'];
 		$docset = $doxygen['docset'];
 		$html = $doxygen['html'];
 		
-		$path = implode(DIRECTORY_SEPARATOR,[
-			dirname( $constants ),
-			$doxygen['output'],
-			$html['output']
-		]); 
+		$constants = file_exists($constants) ? file_get_contents($constants) : '';
+		$output = Frontend::realPath( $doxygen['workdir'], $doxygen['output'] ); 		
 		
-		$addition = [];
-		foreach([
+		$addition = ArrayUtility::joinAssoc([
 			'DOXYFILE_ENCODING' => strtoupper(Frontend::getConfig('charset')),
-			'OUTPUT_DIRECTORY' => $doxygen['output'],
+			'WARN_LOGFILE' => $warnings,
+			'OUTPUT_DIRECTORY' => $output,
 			'EXCLUDE' => $doxygen['exclude'],
 			'OUTPUT_LANGUAGE' => $doxygen['language'],
 			'HTML_OUTPUT' => $html['output'],
@@ -66,50 +76,45 @@ class DocumentationController extends \Devworx\AbstractController {
 			'HTML_COLORSTYLE_HUE' => $html['hue'],
 			'HTML_COLORSTYLE_SAT' => $html['saturation'],
 			'HTML_COLORSTYLE_GAMMA' => $html['gamma']
-		] as $k => $v){
-			$addition []= "{$k} = {$v}";
-		}
+		], ' = ',PHP_EOL);
 		
-		$content = file_exists($constants) ? file_get_contents($constants) : '';
-		$addition = implode(PHP_EOL,$addition);
+		if( file_exists($doxyfile) )
+			unlink($doxyfile);
 		
-		if( file_exists($output) )
-			unlink($output);
+		FileUtility::unlinkRecursive( $output );
 		
-		FileUtility::unlinkRecursive( $path );
+		file_put_contents($doxyfile, "{$constants}\r\n{$addition}");
 		
-		file_put_contents($output, "{$content}\r\n{$addition}");
-		
-		return $output;
+		return $doxyfile;
 	}
 	
 	public function generateAction(){
-		$doxygen = Frontend::getConfig('doxygen','bin');
+		$doxygen = Frontend::getConfig('doxygen');
+		$binary = $doxygen['bin'];
 				
 		$info = [
 			'message' => 'doxygen not installed',
-			'doxygen' => false,
+			'binary' => false,
 			'doxyfile' => false,
-			'workdir' => false,
+			'workdir' => '..',
 			'command' => false,
 			'output' => false,
 		];
 		
-		if( file_exists($doxygen) ){
-			$info['doxygen'] = $doxygen;
-			$configFile = $this->prepareDoxyfile();
+		if( file_exists($binary) ){
+			$info['binary'] = $binary;
+			$doxyfile = $this->prepareDoxyfile();
 			$info['message'] = 'doxyfile not found';
-			if( file_exists($configFile) ){
-				$info['doxyfile'] = $configFile;
-				$info['workdir'] = dirname($configFile);
-				$info['command'] = "\"{$doxygen}\" \"{$configFile}\" 2>&1";
+			if( file_exists($doxyfile) ){
+				$info['doxyfile'] = $doxyfile;
+				$info['command'] = "\"{$binary}\" \"{$doxyfile}\" 2>&1";
 				
 				chdir($info['workdir']);
 				exec($info['command'],$output,$returnCode);
 				chdir('Public');
-				
 				$info['message'] = empty($returnCode) ? 'Success' : 'Error';
 				$info['output'] = implode(PHP_EOL,$output);		
+				
 			}
 		}
 		
@@ -119,34 +124,41 @@ class DocumentationController extends \Devworx\AbstractController {
 	}
 	
 	public function showAction(){
-				
-		// Basisverzeichnis außerhalb von DocumentRoot
-		$baseDir = Frontend::path( 
-			Frontend::getConfig('doxygen','output'), 
-			Frontend::getConfig('doxygen','html','output') 
-		);
-		
+
 		// Dateiname aus GET-Parameter
 		$file = $this->request->getArgument('file') ?? 'index.html';
 		if( empty($file) ) $file = 'index.html';
 
-		// Sicherheit: Pfad bereinigen, um Directory Traversal zu verhindern
-		$cleanPath = realpath("{$baseDir}/{$file}");
-	
-		$content = null;
+		$doxygen = Frontend::getConfig('doxygen');
+
+		// Basisverzeichnis außerhalb von DocumentRoot
+		$path = Frontend::path( 
+			$doxygen['workdir'],
+			$doxygen['output'], 
+			$doxygen['html']['output']
+		);
 		
-		if ($cleanPath && file_exists($cleanPath)) {
-			$info = strtolower( pathinfo($file,PATHINFO_EXTENSION) );
-			$mime = self::MIME[$info];
-			header("Content-Type: $mime");
-			
-			$content = file_get_contents($cleanPath);
-			if (preg_match('/\.html?$/i', $cleanPath)) {
-				$dir = dirname($file);
-				$fileName = basename($file);
-				$baseHref = '/help/' . ($dir !== '.' ? $dir . '/' : '') . $fileName;
-				$content = preg_replace('/<head([^>]*)>/i', "<head$1>\n<base href=\"{$baseHref}\">", $content, 1);
-			}
+		if( !is_dir($path) ){
+			$content = getcwd() . ' misses ' . $path;
+			return;
+		}
+		
+		$absolute = realpath( "{$path}\\{$file}" );
+		if( !file_exists($absolute) ){
+			$content = getcwd() . ' cant find ' . $absolute;
+			return;
+		}
+		
+		$content = null;	
+		$info = strtolower( pathinfo($file,PATHINFO_EXTENSION) );
+		$mime = self::MIME[$info];
+		header("Content-Type: $mime");
+		
+		$content = file_get_contents($absolute);
+		if (preg_match('/\.html?$/i', $file)) {
+			$fileName = basename($file);
+			$baseHref = '/help/' . $file;
+			$content = preg_replace('/<head([^>]*)>/i', "<head$1>\n<base href=\"{$baseHref}\">", $content, 1);
 		}
 		
 		$this->view->assign('content',$content);
