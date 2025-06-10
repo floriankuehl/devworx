ddc6ada47e9bc08d547157c5841d2491fb47f317
Florian Kühl <florian@fksweb.de>
2025-06-10 22:06:59 +0200

    Update Navigation.php

--- Resources/Development/Partials/Navigation.php
+++ Resources/Development/Partials/Navigation.php
@@ -1,4 +1,4 @@
 <nav class="position-fixed top-0 end-0">
 	<a class="btn btn-secondary" href="?controller=model&action=editor">Model Editor</a>
-	<a class="btn btn-secondary" href="?controller=login&action=logout">Logout</a>
+	<a class="btn btn-secondary" href="../">Back</a>
 </nav>
