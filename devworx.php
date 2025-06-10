c4b2e79e5a7a7696badb56f65373a5348002b60f
Florian Kühl <florian@fksweb.de>
2025-06-10 14:38:12 +0200

    Update devworx.php
    correct implementation of debug global

--- devworx.php
+++ devworx.php
@@ -1,39 +1,45 @@
 <?php
-  namespace Devworx;
+	namespace Devworx;
 
-  $GLOBALS['DEVWORX'] = [
-    'CONTEXT' => [
-      'frontend' => 'Frontend Context',
-      'api' => 'API Context',
-      'documentation' => 'Documentation Context'
-    ],
-    'DB' => [
-      "localhost",
-      "root",
-      "",
-      "devworx"
-    ],
-    'CFG' => [
-      
-    ],
-    'PATH' => [
-      'ROOT' => '..',
-      'PUBLIC' => '.',
-    ]
-  ];
+	
 
-  spl_autoload_register(function ($class) {
-    $file = $GLOBALS['DEVWORX']['PATH']['ROOT'] . "/Classes/" . str_replace('\\', '/', $class) . ".php";
-    if (file_exists($file)) {
-        require_once $file;
-        return true;
-    }
-    return false;
-  });
+	$GLOBALS['DEVWORX'] = [
+		'CONTEXT' => [
+			'api' => 'API context',
+			'frontend' => 'Frontend context',
+			'documentation' => 'Documentation context',
+			'development' => 'Development context'
+		],
+		'DB' => [
+			"localhost",
+			"root",
+			"",
+			"storytime"
+		],
+		'CFG' => [
+			'debug' => true
+		],
+		'PATH' => [
+			'ROOT' => '..',
+			'PUBLIC' => '.',
+		]
+	];
+
+	ini_set('display_errors', $GLOBALS['DEVWORX']['CFG']['debug']); 
+	ini_set('display_startup_errors', $GLOBALS['DEVWORX']['CFG']['debug']); 
+	error_reporting($GLOBALS['DEVWORX']['CFG']['debug'] ? E_ALL : 0);
+	
+	spl_autoload_register(function ($class) {
+		$file = $GLOBALS['DEVWORX']['PATH']['ROOT'] . "/Classes/" . str_replace('\\', '/', $class) . ".php";
+		if (file_exists($file)) {
+			require_once $file;
+			return true;
+		}
+		return false;
+	});
   
-  //Utility::OPCacheUtility::build();
-  
-  set_exception_handler([Utility\DebugUtility::class,'exception']);
-  
-  $DB = new Database(...$GLOBALS['DEVWORX']['DB']);
-  if( !$DB->connected() ) $DB->connect();
+	//Utility::OPCacheUtility::build();
+	set_exception_handler([Utility\DebugUtility::class,'exception']);
+
+	$DB = new Database(...$GLOBALS['DEVWORX']['DB']);
+	if( !$DB->connected() ) $DB->connect();
