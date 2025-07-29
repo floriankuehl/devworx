71a873a433bd46fbbb2e00e86d29c374443d8d27
Florian Kühl <florian@fksweb.de>
2025-07-29 08:58:07 +0200

    Add files via upload

---
+++ Context/Documentation/Classes/Controller/DocumentationController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Documentation\Controller;
+
+use \Devworx\Utility\DebugUtility;
+use \Documentation\Utility\DoxygenUtility;
+
+class DocumentationController extends \Devworx\Controller\AbstractController {
+	
+	public function initialize(): void {
+		$this->setBlockLayout(true);
+	}
+	
+	public function generateAction(){
+		$result = DoxygenUtility::Doxygen();
+		$result = DebugUtility::var_dump( $result );
+		$this->view->assign('content',$result);
+	}
+	
+	public function showAction(){
+		$file = $this->request->getArgument('file') ?? 'index.html';
+		if( empty($file) ) $file = 'index.html';
+
+		$content = DoxygenUtility::Route($file);
+		$this->view->assign('content',$content);
+	}
+	
+}
